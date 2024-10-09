dfe62d94a9f1b81d81ff8529a5ef89ffe1f04ce7
nihility <nemaniaon@gmail.com>
2024-10-09 03:51:15 +0200

    5

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { sveltekit } from "@sveltejs/kit/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		sveltekit(),
-		// basicSsl()
-	],
-	server: {
-		host: true,
-		// https: true,
-		port: 3000
-	}
+  plugins: [
+    sveltekit(),
+    // Uncomment the line below if you need SSL during development
+    // basicSsl()
+  ],
+  server: {
+    host: true,
+    // https: true, // Uncomment this to enable HTTPS in development
+    port: 3000,
+  },
+  build: {
+    outDir: "build", // This specifies the output directory
+  },
 };
 
 export default config;
-
-export default {
-  build: {
-    outDir: 'build', // or 'dist' or 'public', depending on your setup
-  },
-};
